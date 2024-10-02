e8cf2837de0738d42eff9f72628815a230ca8e7e
m0slah <muhammedslahqadir@gmail.com>
2024-10-02 21:59:47 +0300

    connect MongoDb with Express

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+const uri =
+  "mongodb+srv://muhammedslah098765:CBERV2drp57oNV4j@cluster0.7yd32.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-app.get("/", (req, res) => {
-  res.send("hello from node API server");
+async function connnect(params) {
+  try {
+    await mongoose.connect(uri);
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+connnect();
+app.listen(8000, () => {
+  console.log("Server Started on 8000");
 });
