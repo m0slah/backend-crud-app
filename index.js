60d5ed976f8c5ee98454d7516851213e2b98dd6d
m0slah <muhammedslahqadir@gmail.com>
2024-10-05 04:38:39 +0300

    create products post request

--- index.js
+++ index.js
@@ -3,6 +3,25 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 
+const Product = require("./models/products.model");
+const { message } = require("antd");
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello from Node API server update");
+});
+
+app.post("/products", async (req, res) => {
+  // const { name, price, quantity, image } = req.body;
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
 mongoose
   .connect(process.env.MONGO_URI, { connectTimeoutMS: 30000 })
   .then(() => {
