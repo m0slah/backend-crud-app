90303468b87949709ffbf69ea626b983a57bb1d5
MO Slah <88237350+m0slah@users.noreply.github.com>
2025-03-11 11:23:42 +0300

    add middleware for JSON and URL-encoded data parsing

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const productRoute = require("./routes/product.route");
 const app = express();
+
+//middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
